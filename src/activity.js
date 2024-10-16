61173a4b0d342e845cb48bf79623c5cb5b455b87
Cristo Acorán <cristo27603@gmail.com>
2024-10-16 13:53:09 +0100

    nn

--- src/activity.js
+++ src/activity.js
@@ -3,11 +3,11 @@ const Service = require('./service')
 class Activity extends Service {
   #instructor = null
 
-  constructor (name, fixedCost = 0, assistance = 0, variableCost = 0) {
+  constructor (name, fixedCost = 0, variableCost = 0, assistance = 0) {
     super(name)
     this.fixedCost = fixedCost
-    this.variableCost = variableCost
     this.assistance = assistance
+    this.variableCost = variableCost
   }
 
   calculateCost () {
